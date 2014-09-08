eec50b2ef43c3646e8fa7deb94fbf43ded8e3eab
Leonardo De Caria <decarialeonardo@gmail.com>
2014-09-08 18:24:02 -0300

    Inicio del ambiente de Front End

---
+++ front/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name frontGeolocacionApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the frontGeolocacionApp
+ */
+angular.module('frontGeolocacionApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ front/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name frontGeolocacionApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the frontGeolocacionApp
+ */
+angular.module('frontGeolocacionApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
