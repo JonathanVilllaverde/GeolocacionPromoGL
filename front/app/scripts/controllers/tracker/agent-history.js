387e13c48bab4b0c66712ca976aec35bc4c1300e
Leonardo De Caria <decarialeonardo@gmail.com>
2014-09-24 19:36:37 -0300

    Integracion de los servicios rest con el front

--- front/app/scripts/controllers/tracker/agent-history.js
+++ front/app/scripts/controllers/tracker/agent-history.js
@@ -8,8 +8,49 @@
  * Controller of the geolocacionApp
  */
 angular.module('geolocacionApp')
-  .controller('AgentHistoryCtrl',  ['$scope', function ($scope) {
-   		$scope.open = function(){
-   			console.log('here');
-   		}
+  .controller('AgentHistoryCtrl',  ['$scope','APITrackerService','$modal', function ($scope, APITrackerService, $modal) {
+
+  		$scope.items = [];
+
+   		$scope.showHistory = function(event){
+
+   			var onSuccess = function(data){
+		      $scope.items = data;
+		      showModal();
+		    };
+
+		    var onError = function(){
+		      console.log('error');
+		    };
+
+		    // HARCODE
+		    //event.target.id = '542325cee4b0e1b339fc39dc';
+
+    		APITrackerService.getAgentHistory(event.target.id ,onSuccess, onError);
+		};
+
+		var showModal = function(){
+			var modalInstance = $modal.open({
+		      templateUrl: 'myModalContent.html',
+		      controller: ModalInstanceCtrl,
+		      resolve: {
+		        items: function () {
+		          return $scope.items;
+		        }
+		      }
+		    });
+		};
+
+		var ModalInstanceCtrl = function ($scope, $modalInstance, items) {
+		  $scope.items = items;
+
+		  $scope.ok = function () {
+		    $modalInstance.close($scope.selected.item);
+		  };
+
+		  $scope.cancel = function () {
+		    $modalInstance.dismiss('cancel');
+		  };
+		};
+
   }]);
